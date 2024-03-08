e7f21d4b39dbb857b77c60b11b283ab10e3c080e
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-03-08 21:50:19 +0300

    edited App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -146,7 +146,7 @@ public class App{
             } catch (NumberFormatException e) {
                 // Hatalı giriş durumunda 400 Bad Request döndür
                 res.status(400);
-                return;
+                return null; // Hata durumunda null döndür
             }
         
             // Başarı durumunu değerlendir
@@ -156,7 +156,7 @@ public class App{
             Map<String, Object> map = new HashMap<>();
             map.put("isSuccess", isSuccess);
             return new ModelAndView(map, "compute.mustache");
-        }, new MustacheTemplateEngine());        
+        }, new MustacheTemplateEngine());
     }
 
     static int getHerokuAssignedPort() {
@@ -164,6 +164,6 @@ public class App{
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
-        return 4567; // Return default port if heroku-port isn't set (i.e. on localhost)
+        return 4567; 
     }
 }
